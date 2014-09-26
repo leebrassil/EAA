8bae755bea8132d6685084c77d198272856503ce
leebrassil <leebrassil@hotmail.com>
2014-09-26 15:45:07 +0100

    made cpuscript runnable

--- week2/cpuscript.sh
+++ week2/cpuscript.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#: Title : cpucount.sh
+#: Date : 25/9/14
+#: Author : Lee Brassil
+#: Version : 1.0
+#: Description : cpu count script
+#: Options :
 grep 'processor' /proc/cpuinfo | wc -l
